be315d15aab189d2702f3c6f7b617dc43e9368c4
jonak-dev <jonak.dev@gmail.com>
2025-10-14 23:44:19 +0800

    feat: add media url

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = str(BASE_DIR / 'media/files')
+MEDIA_URL = '/media/'
